20683c71bd3e8697ddc160720afd36fb8347cebf
ilkerymz <iilker0053@gmail.com>
2022-12-13 15:07:44 +0300

    adress has been updated, join us link updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
-import { useTranslation } from 'react-i18next';
-import { SeperatorDownWhite } from './seperators';
+import { useTranslation } from "react-i18next";
+import { SeperatorDownWhite } from "./seperators";
 
 const Footer = ({ Seperator = SeperatorDownWhite }) => {
   const { t } = useTranslation();
@@ -14,29 +14,22 @@ const Footer = ({ Seperator = SeperatorDownWhite }) => {
       <div class="container-fluid text-center last-div-container-fluid">
         <div class="container-last-div container">
           <hr class="contact-border" />
-          <h1 class="h1-last-div">{t('contact').toUpperCase()}</h1>
-          <hr style={{ height: 1, backgroundColor: 'rgb(151,103,223)' }} />
+          <h1 class="h1-last-div">{t("contact").toUpperCase()}</h1>
+          <hr style={{ height: 1, backgroundColor: "rgb(151,103,223)" }} />
           <div
             class="row-last-div row justify-content-start"
             style={{ marginBottom: 40, marginTop: 40 }}
           >
             <div class="col-last-div col text-right">
               <a href="http://www.gtu.edu.tr/">
-                <h2 class="h2-last-div">{t('gebze technical university')}</h2>
+                <h2 class="h2-last-div">{t("gebze technical university")}</h2>
               </a>
               <a
                 target="_blank"
                 class="index-first-div-linked2"
-                href="https://www.google.com.tr/maps/place/Cumhuriyet,+2254.+Sk.+No:2,+41400+Gebze%2FKocaeli/@40.8091319,29.3628288,17z/data=!3m1!4b1!4m5!3m4!1s0x14cade56ad9be727:0x6f7e7fd7b66e8326!8m2!3d40.8091279!4d29.3650175"
+                href="https://www.google.com/maps/place/Cumhuriyet,+GTU+Rover+At%C3%B6lyesi,+41400+Gebze%2FKocaeli/data=!4m2!3m1!1s0x14cadf43c7ab52d5:0x5b0b8f71cad0e0eb?utm_source=mstt_1&entry=gps&g_ep=CAESCjExLjU3LjQ4MDEYACCIJyoAQgJUUg%3D%3D"
               >
-                <p class="p-last-div">
-                  Cumhuriyet, 2254. Sk. No:2, 41400 Gebze/Kocaeli
-                </p>
-              </a>
-              <a class="mailto" href="mailto:rover@gtu.edu.tr">
-                <p style={{ color: 'white' }} class="p-last-div last-p">
-                  rover@gtu.edu.tr
-                </p>
+                <p class="p-last-div">{t("adress")}</p>
               </a>
             </div>
             <div class="col-lg-6 col-sm-12">
