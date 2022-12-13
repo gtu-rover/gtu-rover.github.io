a00689efd29c5d60d6263d6af38eb2db77b7ef67
ilkerymz <iilker0053@gmail.com>
2022-12-13 15:07:44 +0300

    adress has been updated, join us link updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,39 @@
-import { Link } from 'react-router-dom';
-import { LanguageSwitch } from './LanguageSwitch';
+import { Link } from "react-router-dom";
+import { LanguageSwitch } from "./LanguageSwitch";
 
 const Navbar = () => {
   return (
     <div>
       <nav class="navbar navbar-light fixed-top">
-        <Link to="/">
-          <div class="navbar-brand">
+        <div class="navbar-brand">
+          <a
+            class="navbar-brand"
+            href="#"
+            style={{
+              float: "left",
+              textAlign: "center",
+            }}
+          >
             <img id="navbar-img1" src="/images/logo/logo.png" alt="" />
-          </div>
-        </Link>
+          </a>
+        </div>
 
         <div
           style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <div style={{ float: 'left' }}>
+          <div style={{ float: "left" }}>
             <LanguageSwitch />
           </div>
           <a
             class="navbar-brand"
             href="#"
             style={{
-              float: 'right',
-              textAlign: 'center'
+              float: "right",
+              textAlign: "center",
             }}
           >
             <img id="navbar-img2" src="/images/lets-white2.png" alt="" />
